0826dc04ce024a7a957f5d284d71690f4b986254
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-10-18 11:54:18 -0500

    adding initial code

---
+++ src/main/java/hello/example/Hello.java
@@ -0,0 +1,11 @@
+package hello.example;
+
+public class Hello {
+    public static void main(String [] args){
+        System.out.println("hello World!");
+        // variables, sequence , iteration, and selection
+        // Variables
+        int x = 99;
+
+    }
+}
