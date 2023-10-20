5f5fb81d38f5693f1e53c34881920d7da0118019
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-10-20 11:38:51 -0500

    Adding simple conversion

---
+++ src/main/java/ConversionClass.java
@@ -0,0 +1,10 @@
+public class ConversionClass {
+    public static void main (String [] Args){
+        double myDouble = 3.14;
+        float myFloat = 3.14f; // conversion / lossy conversion
+        double yourDouble = myFloat; // widening / lossless conversion
+        /*
+        The 8 primitive types : int, float , double, char, boolean, byte, short , long
+         */
+    }
+}
