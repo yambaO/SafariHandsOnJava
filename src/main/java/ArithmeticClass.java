034c62b110423080f1eaac00f4e326813191c3d9
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-10-20 12:05:54 -0500

    Adding operators

---
+++ src/main/java/ArithmeticClass.java
@@ -0,0 +1,33 @@
+public class ArithmeticClass {
+    public static void main (String[] Args){
+        int a = 10;
+        int b = 15;
+
+       // binary operators
+        int result = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = b / a;
+        int remainder = b % a;
+        System.out.println("Result is  " + result);
+        System.out.println("Difference  is  " + difference);
+        System.out.println("product is  " + product);
+        System.out.println("quotient is " + quotient);
+        System.out.println("Quotient is " + remainder);
+
+        result += 20; // result = result + 20
+        System.out.println("Result is now " + result);
+
+        product *= 2; // product = product * 2
+
+        System.out.println("Product is now " + product);
+
+        // unary operators
+        result ++; // result = result + 1
+        System.out.println("Result++ is " + result);
+
+        result --; // result = result - 1
+        System.out.println("Result-- is " + result);
+
+    }
+}
