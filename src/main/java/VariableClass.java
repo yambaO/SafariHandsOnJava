abfb86964955ba568320957c1d4d49c71d1fb15b
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-10-20 11:24:43 -0500

    Adding basic variables

---
+++ src/main/java/VariableClass.java
@@ -0,0 +1,11 @@
+public class VariableClass {
+    public static void main(String [] args){
+        int  myAge; // declaration
+        myAge = 19; // initialization
+        String name = "Mike";
+        String hometown = "Texas";
+        final int someNumber = 150;
+        System.out.println(name + " is " + myAge);
+        System.out.print(name + " is " + myAge + '\n' +  "and live in " + hometown);
+    }
+}
