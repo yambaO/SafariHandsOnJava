64050129991610d19f69a823df4d57ce4b0c6661
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-10-20 11:45:35 -0500

    Adding comment type

---
+++ src/main/java/CommentClass.java
@@ -0,0 +1,15 @@
+public class CommentClass {
+    /*
+         learning some java 101
+         This is a multiline comment
+         Enjoying the course
+        */
+
+    /*
+    Main is the entry point of the application
+     */
+    public static void main (String [] Args){
+       System.out.println(" Comment are good for coding  best practice");
+        // single line comment
+    }
+}
