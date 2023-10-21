77362498adaeca4e26f454f3126c8e3eadea34bb
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-10-20 20:36:17 -0500

    Adding user input

---
+++ src/main/java/UserInputClass.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class UserInputClass {
+    public static void main (String[] args){
+        Scanner Keyboard = new Scanner(System.in);
+        String name;
+        int age;
+        String city;
+        double realNumber;
+
+        System.out.println("What is your name?");
+        name = Keyboard.nextLine();
+        System.out.println("What is your age?");
+        age =  Keyboard.nextInt();
+        Keyboard.nextLine();
+        System.out.println("What is the real number?");
+        realNumber = Keyboard.nextDouble();
+        Keyboard.nextLine();
+        realNumber *= 2;
+        System.out.println("What is the city where you live?");
+        city = Keyboard.nextLine();
+
+        System.out.println("Hello, " + name );
+        System.out.println(" Age is " + age);
+        System.out.println("the real number is " + realNumber);
+        System.out.println("my city is "+ city);
+    } //end main
+}
