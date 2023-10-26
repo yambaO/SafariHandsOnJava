4c1f10c460c340533c59f2439ca711576ae8b4f6
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-10-26 11:43:17 -0500

    Adding project 3

--- src/main/java/Randomnumber/Proj3_1_LearningPackages.java
+++ src/main/java/Randomnumber/Proj3_1_LearningPackages.java
@@ -6,22 +6,22 @@ public class Proj3_1_LearningPackages {
         int numberOfCoursesEnrolled;
         int totalPrice;
         Scanner keyboard = new Scanner(System.in);
-        System.out.print("Please provide the type of package you have:\t");
+        System.out.print("Which package do you have? 1 , 2 or 3:\t");
         packageName = keyboard.nextLine();
-        System.out.print("How many courses did you enrolled? : \t");
+        System.out.print("How many courses did you enrolled? Package: \t");
         numberOfCoursesEnrolled = keyboard.nextInt();
         switch (packageName){
-            case "Learning Package 1":
+            case "1":
                 totalPrice = (numberOfCoursesEnrolled * 6) + 10;
-                System.out.println("The total price for learning package 1 this month is: " + totalPrice + "$");
+                System.out.println("The total price for learning package 1 this month is: " + "$" + totalPrice);
                 break;
-            case "Learning Package 2":
+            case "2":
                 totalPrice = (numberOfCoursesEnrolled * 4) + 12;
-                System.out.println("The total price for learning package 2 this month is: " + totalPrice  + "$");
+                System.out.println("The total price for learning package 2 this month is: " + "$" + totalPrice);
                 break;
-            case "Learning Package 3":
+            case "3":
                 totalPrice = (numberOfCoursesEnrolled * 3) + 15;
-                System.out.println("The total price for learning package 3 this month is: " + totalPrice  + "$");
+                System.out.println("The total price for learning package 3 this month is: " + "$" + totalPrice);
                 break;
             default :
                 System.out.println("You have entered an invalid package");
