8e22b65749b0dd48df63b2ae556ac64ead2baf7b
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-11-03 19:58:49 -0500

    adding stringbuilder

---
+++ src/main/java/StringMethods/StringBuilderClass.java
@@ -0,0 +1,18 @@
+package StringMethods;
+
+public class StringBuilderClass {
+    public static void main(String[] args){
+      StringBuilder sb = new StringBuilder("John Baugh");
+      sb.append(" is awesome");
+      System.out.println(sb);
+
+      sb.insert(5, "Phillip ");
+      System.out.println(sb);
+
+      sb.replace(22, 29,"amazing" );
+      System.out.println(sb);
+
+      sb.delete(5,13);
+      System.out.println(sb);
+    } //end of main
+}
